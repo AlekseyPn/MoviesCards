4f4114154ebbc3c15ec53f17bdcf0ff06205da8c
Fargustian <aleksfers@icloud.com>
2018-05-26 12:50:23 +0500

    hound fix

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
+# movie model
 class Movie < ApplicationRecord
-    validates :title, :year, :revision_date, :director, presence: true
-    validates :year, numericality: {only_integer: true}
-    validates :title, :director, length: {minimum: 2}
-    before_validation :set_revision_date, on: [:create, :update]
+  validates :title, :year, :revision_date, :director, presence: true
+  validates :year, numericality: { only_integer: true }
+  validates :title, :director, length: { minimum: 2 }
+  before_validation :set_revision_date, on: %i[create update]
 
-    private
-    def set_revision_date
-        self.revision_date = DateTime.now.strftime("%d %B")
-    end
+  private
+
+  def set_revision_date
+    self.revision_date = Time.zone.today.strftime('%d %B')
+  end
 end
